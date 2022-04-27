98085b5c903b0e840b57ce9d883cc50620473bc6
Арман Чархчян <re-mont1@mail.ru>
2022-04-28 01:08:32 +0300

    fix

--- Sources/Managers/LocalStorageServices/CoreDataService/CoreDataService.swift
+++ Sources/Managers/LocalStorageServices/CoreDataService/CoreDataService.swift
@@ -24,7 +24,7 @@ public final class CoreDataService {
     private let fileName: String
     
     public init(fileName: String) {
-        self.fileName = "Model"//fileName
+        self.fileName = fileName
     }
     
     private var context: NSManagedObjectContext {
