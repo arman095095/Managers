46a428a1db18a118fd8f626bee27453c70dce210
Арман Чархчян <re-mont1@mail.ru>
2022-04-29 01:23:17 +0300

    fix

--- Sources/Managers/AppManagers/AccountManager/AccountManagerAssembly.swift
+++ Sources/Managers/AppManagers/AccountManager/AccountManagerAssembly.swift
@@ -28,6 +28,6 @@ public enum AccountManagerAssembly {
                                   quickAccessManager: quickAccessManager,
                                   profileService: profileService,
                                   cacheService: cacheService)
-        }
+        }.inObjectScope(.weak)
     }
 }
