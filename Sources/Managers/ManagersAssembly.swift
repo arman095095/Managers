4ef3af463180598075fb76909dd904f6e6d35670
Арман Чархчян <re-mont1@mail.ru>
2022-05-05 14:29:34 +0400

    fix

--- Sources/Managers/ManagersAssembly.swift
+++ Sources/Managers/ManagersAssembly.swift
@@ -13,6 +13,10 @@ public final class ManagersAssembly: Assembly {
     public init() { }
     
     public func assemble(container: Container) {
+        KeychainServiceAssembly().assemble(container: container)
+        UserDefaultsServiceAssembly().assemble(container: container)
+        CoreDataServiceAssembly().assemble(container: container)
+        AccountCacheServiceAssembly().assemble(container: container)
         QuickAccessManagerAssembly().assemble(container: container)
         AuthManagerAssembly().assemble(container: container)
         AccountManagerAssembly().assemble(container: container)
